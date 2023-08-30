477c92b1bca08f40a4548d45af1a922bb5383b26
Shivam <kpsshivam.gondxi@gmail.com>
2023-08-31 00:25:07 +0530

    latest commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import {Server} from 'socket.io'
+
+
+const PORT = 9000;
+
+const io = new Server(PORT , {
+  cors:{
+    origin:'http://localhost:3000',
+    method:['GET','POST']
+  }
+});
+
+io.on('connection',socket=>{
+    socket.on('send-changes',delta =>{
+        socket.broadcast.emit('receive-changes',delta);
+    })
+});
